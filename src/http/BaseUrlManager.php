5f54dbcc32641da50cfc98b9f04a19be18f675de
1023150697@qq.com <1023150697@qq.com>
2021-08-09 20:48:32 +0800

    update

---
+++ src/http/BaseUrlManager.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace toom1996\http;
+
+
+use toom1996\base\Component;
+
+/**
+ * Class BaseUrlManager
+ *
+ * @author TOOM <1023150697@qq.com>
+ * 
+ */
+abstract class BaseUrlManager extends Component
+{
+
+    /**
+     * Route adapter.
+     * @var
+     */
+    protected $adapter;
+
+    /**
+     *
+     *
+     * @return mixed
+     */
+    abstract protected function parseRequest(): array ;
+
+    /**
+     * Returns
+     * @param $config
+     *
+     * @return array
+     */
+    protected static function getRoute($config): array
+    {
+        return $config['components']['urlManager']['route'];
+    }
+    
+}
