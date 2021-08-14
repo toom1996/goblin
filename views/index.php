d925b6ab134227dfc9fe24785eae7977ad993837
1023150697@qq.com <1023150697@qq.com>
2021-08-14 16:48:26 +0800

    update

---
+++ views/index.php
@@ -0,0 +1,20 @@
+<?php
+$this->title = 'Goblin framework!';
+?>
+<style>
+    body {
+        background-color: #d2cbe4;
+    }
+
+    .c-header-box {
+        text-align: center;
+        padding: 60px 25px 50px;
+        position: relative;
+    }
+
+</style>
+<div class="container">
+    <header class="c-header-box">
+        <h2 class="c-header text">Tooltip &amp; Popover <br> Positioning Engine~</h2>
+    </header>
+</div>
---
+++ views/layouts/main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?= YiiS::$app->charset ?>" />
+    <meta http-equiv=X-UA-Compatible content="IE=edge,chrome=1">
+    <meta name=viewport content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+    <title><?= \toom1996\helpers\HtmlHelper::encode($this->title) ?></title>
+</head>
+<body>
+<?= $content ?>
+<script async="" src="https://www.google-analytics.com/analytics.js"></script>
+</body>
+</html>
